e6309fda8693753c99d7bee5ca89e985c12ddfb7
zineddineAyoub <58141094+zineddineAyoub@users.noreply.github.com>
2021-02-06 12:40:35 +0100

    Update OauthService.java

--- src/main/java/ensa/pay/service/OauthService.java
+++ src/main/java/ensa/pay/service/OauthService.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import java.util.List;
 
 //@FeignClient(name="user-service",url="http://ensaspay-zuul-gateway.herokuapp.com/oauth/api/user")
-@Service
+//@Service
 public interface OauthService {
 
 
