14645fec08c33c88fb63362cb7bd0a981ff1a5f3
zineddineAyoub <58141094+zineddineAyoub@users.noreply.github.com>
2021-02-06 12:37:17 +0100

    Update EnsaProjetAgentApplication.java

--- src/main/java/ensa/pay/EnsaProjetAgentApplication.java
+++ src/main/java/ensa/pay/EnsaProjetAgentApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
+//@EnableFeignClients
 public class EnsaProjetAgentApplication {
 
 	public static void main(String[] args) {
